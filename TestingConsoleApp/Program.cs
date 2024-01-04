328f35e8da72153f291a94bfb015963d93795f51
Slobodan Kuzmanovic <slobodan.kuzmanovic@qualifacts.com>
2024-01-04 16:34:37 +0100

    Change folder for cs

--- TestingConsoleApp/Program.cs
+++ TestingConsoleApp/Program.cs
@@ -6,12 +6,6 @@ using static System.Net.WebRequestMethods;
 
 Console.WriteLine("Hello, World!");
 
-int[] a = { 5, 7, 10, 5, 15 };
-int[] b = { 2, 2, 1, 3, 5 };
-
-int steps = StepCountToEqualElementOfArray.MinStepsToMakeEqual(a, b);
-Console.WriteLine($"Minimum steps to make all elements equal: {steps}"); 
-
 
 
 Console.ReadKey();
